78cd146d6e7d27225bc671f848addc60fb4f0da0
Nahuel <vallesnahuel1998@gmail.com>
2020-10-21 19:46:12 -0300

    Copia de seguridad
    Realizo copia de una versión funcional antes de probar cambios

--- js/cart.js
+++ js/cart.js
@@ -45,6 +45,10 @@ document.addEventListener("DOMContentLoaded", function (e) {
                 </div>
             `
                 document.getElementById("subTotal").innerHTML = subTotal + " " + "UYU";
+                let totalCost = (subTotal * formulario.value) / 100;
+                sendCostHTML.innerHTML = totalCost + " UYU"
+                totalCostHTML.innerHTML = subTotal + totalCost + " UYU";
+
             }
             for (let i = 0; i < cartArticle.length; i++) {
                 const element = cartArticle[i];
@@ -83,7 +87,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
         purchaseButton.addEventListener("click", function () {
             purchaseInformation.innerHTML = `
             <!-- Modal -->
-            <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1"
+              aria-labelledby="staticBackdropLabel" aria-hidden="true">
               <div class="modal-dialog">
                 <div class="modal-content">
                   <div class="modal-header">
@@ -93,17 +98,28 @@ document.addEventListener("DOMContentLoaded", function (e) {
                     </button>
                   </div>
                   <div class="modal-body">
-                   ---------------------------------------------ACA VA EL FORM PARA PEDIR DATOS DE PAGO------------------------------------
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-primary">Aceptar</button>
+                    <form method="GET">
+                      <input type="radio" id="VISA" name="creditCard" value="VISA" required>
+                      <label for="visa">VISA</label><br>
+                      <input type="radio" id="masterCard" name="creditCard" value="MasterCard">
+                      <label for="masterCard"> MasterCard </label><br>
+                      <input type="text" placeholder="Nº de Tarjeta" pattern="\\d{13}|\\d{16}" name="cardNumber" required>
+                      <input minlength="3" maxlength="3" size="5" type="text" placeholder="CVV" name="cardCVV" required>
+                      <input minlength="1" maxlength="2" size="5" type="number" placeholder="Mes" min="1" max="12" name="month"
+                        required>
+                      <input minlength="4" maxlength="4" size="5" type="text" placeholder="Año" pattern="202([0-9])" name="year"
+                        required>
+                      <hr>
+                      <input type="radio" id="" name="transfer" value="True">
+                      <label for="transfer">Prefiero hacer una transferencia...</label>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-primary">Aceptar</button>
+                    </form>
                   </div>
                 </div>
               </div>
-            </div>
             `
         })
-
     })
 });
